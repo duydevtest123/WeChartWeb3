07fd1dd97b8ca371e7998002c8b3abb074ca4aa7
lamnt95 <34705561+lamnt95@users.noreply.github.com>
2023-08-19 10:47:48 +0700

    a

--- server/config/database.js
+++ server/config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-  url: 'mongodb://localhost:27017/charting_56',
+  url: 'mongodb+srv://doadmin:8P5N0gcEfK2763G9@db-mongodb-sgp1-61663-081b44e7.mongo.ondigitalocean.com/admin?tls=true&authSource=admin',
 };
